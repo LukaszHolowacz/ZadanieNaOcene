03127a4b77637d23baaf15136b5be9a7330f3f57
palczyk69 <lukasz.holowacz5@gmail.com>
2021-10-28 12:21:41 +0200

    1 commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import org.w3c.dom.Text
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.textbutton).setOnClickListener {
+            findViewById<TextView>(R.id.textView).text =
+                findViewById<EditText>(R.id.PoleTekstowe).text
+        }
+    }
+}
