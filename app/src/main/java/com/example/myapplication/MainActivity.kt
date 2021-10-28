f48fa33ec6eeade798db69fc4a465d69a90beda2
palczyk69 <lukasz.holowacz5@gmail.com>
2021-10-28 13:07:35 +0200

    sumowanie, proba laczenia tekstu, przyciski, pola tekstowe

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -5,16 +5,31 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
-import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {//
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        var liczbaPierwsza=0
+        var liczbaDruga=0
+        var wynik=0
+
         findViewById<Button>(R.id.textbutton).setOnClickListener {
-            findViewById<TextView>(R.id.textView).text =
+            findViewById<TextView>(R.id.zastapionytext).text =
                 findViewById<EditText>(R.id.PoleTekstowe).text
+        }
+        findViewById<Button>(R.id.dopisztekstbutton).setOnClickListener {
+
+        }
+        findViewById<Button>(R.id.sumabtn).setOnClickListener {
+            liczbaPierwsza = findViewById<EditText>(R.id.liczba1pole).text.toString().toInt()
+            liczbaDruga = findViewById<EditText>(R.id.liczba2pole).text.toString().toInt()
+
+            wynik = liczbaPierwsza + liczbaDruga
+
+            findViewById<TextView>(R.id.wynikDodawania).text =
+                liczbaPierwsza.toString() + " + " + liczbaDruga.toString() + " = " + wynik.toString()
         }
     }
 }
